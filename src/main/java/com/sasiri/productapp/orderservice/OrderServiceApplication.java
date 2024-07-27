e59b1f0db9105605bc6de04f78b721aae60dba8a
sasiri-jambugaswatta <jambugaswattasasiri@gmail.com>
2024-07-27 11:40:33 +0530

    Aggregate Documentation to gateway endpoint and remove OpenFeign and implement restClient

--- src/main/java/com/sasiri/productapp/orderservice/OrderServiceApplication.java
+++ src/main/java/com/sasiri/productapp/orderservice/OrderServiceApplication.java
@@ -2,7 +2,6 @@ package com.sasiri.productapp.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 public class OrderServiceApplication {
